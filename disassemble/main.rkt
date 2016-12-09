922bdaf2720bfdbc672caada83a2391832a31bc4
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2016-12-09 15:32:26 -0500

    Fixes to handle different versions better.

--- disassemble/main.rkt
+++ disassemble/main.rkt
@@ -7,18 +7,22 @@
 
 (lazy-require ("nasm.rkt" [nasm-disassemble]))
 
-(provide dump disassemble (rename-out [disassemble decompile]))
+(provide dump typeof disassemble (rename-out [disassemble decompile]))
 
 (define _mz_hash_key _short)
 (define _mzshort _int)
 
-;; conditionally detect this based on the Racket version
-(define prim-type-number
-  (cond [(version<=? (version) "6.2.1")
-         38]
-        [(version<? (version) "6.4.0.8")
-         39]
-        [else 40]))
+
+;; this struct is just to help get the start tag for procedures
+(define-cstruct _test_scheme_object
+  ([typetag _short]
+   [key _mz_hash_key]))
+
+(define (get-tag-num v)
+  (test_scheme_object-typetag 
+   (cast v _scheme _test_scheme_object-pointer)))
+
+(define prim-type-number (get-tag-num values))
 
 (define _scheme_type
   (_enum ;; from stypes.h
@@ -40,13 +44,13 @@
 ;; we assume that we're always in precise-gc mode
 (define _scheme_inclhash_object _scheme_object)
 
-(define-cstruct _scheme_closure_data
+(define-cstruct _scheme_lambda
   ([iso _scheme_inclhash_object]
    [num_params _mzshort]
    [max_let_depth _mzshort]
    [closure_size _mzshort]
    [closure_map (_cpointer _mzshort)]
-   [code _scheme]
+   [body _scheme]
    [name _scheme]
    [tl_map _gcpointer]
    ;; more fields here for JIT
@@ -54,21 +58,19 @@
 
 (define-cstruct _scheme_closure
   ([so _scheme_object]
-   [code _scheme_closure_data-pointer]
+   [code _scheme_lambda-pointer]
    [vals _scheme]))
 
-(define _scheme_closed_prim (_fun _pointer _int (_or-null _scheme_object-pointer) -> _scheme))
-
-(define-cstruct _native_closure_data
+(define-cstruct _native_lambda
   ([iso _scheme_inclhash_object]
-   [code _fpointer]
+   [start_code _fpointer]
    ;; either a void * tail_code (non-case-lambda) or mzshort * arities (case-lambda)
    [u _gcpointer]
    [arity_code _gcpointer]
    [max_let_depth _mzshort]
    [closure_size _mzshort]
    ;; either a
-   ;; struct Scheme_Closure_Data *orig_code; /* For not-yet-JITted non-case-lambda */ or
+   ;; struct Scheme_Lambda *orig_code; /* For not-yet-JITted non-case-lambda */ or
    ;; Scheme_Object *name;
    [name _scheme]
    [tl_map _gcpointer]
@@ -77,10 +79,9 @@
 
 (define-cstruct _scheme_native_closure
   ([so _scheme_object]
-   [code _native_closure_data-pointer]
+   [code _native_lambda-pointer]
    [vals _pointer]))
 
-(define on_demand_jit_code (get-ffi-obj "scheme_on_demand_jit_code" #f _pointer))
 (define find_jit_code_end (get-ffi-obj "scheme_jit_find_code_end" #f
                                        (_fun _gcpointer -> _gcpointer)))
 
@@ -94,13 +95,13 @@
     (raise-argument-error name "procedure" f))
   (jit-now! f)
   (define fp (cast f _scheme _scheme_native_closure-pointer))
-  (unless (eq? 'native_closure_type (scheme_object-typetag fp))
+  (unless (eq? 'native_closure_type (typeof fp))
     (raise-argument-error name "non-primitive procedure" f))
   (match (scheme_native_closure-code fp)
-    [(native_closure_data iso code u arity-code max-let-depth closure-size nm tl_map retained)
+    [(native_lambda iso code u arity-code max-let-depth closure-size nm tl_map retained)
      (let* ([case? (< closure-size 0)]
             [closure-size (if case?
-                              (- (add1 closure-size))
+                              (sub1 (- closure-size))
                               closure-size)]
             [tail-code (if case? #f u)]
             [num-arities (if case? closure-size #f)]
@@ -116,7 +117,8 @@
        (unless tail-code
          (error name "unable to read jitted code"))
        (unless (or end size)
-         (error name "unable to find the end of the jitted code, and no #:size supplied"))
+         (error name
+                "unable to find the end of the jitted code, and no #:size supplied"))
        (cast tail-code _pointer (_bytes o size)))]))
 
 (define systype (if (fixnum? (expt 2 61)) 64 32)) ;; no 16-bit racket
@@ -132,8 +134,11 @@
                      color #f 0 '())]))
 
 
+(provide get-code-bytes)
+(define (get-code-bytes f) (go 'get-code-bytes f))
+
 (define (dump f file-name)
-  (define bs (go 'decompile f))
+  (define bs (go 'dump f))
   (let ((file (open-output-file file-name #:exists 'replace)))
     (write-bytes bs file)
     (close-output-port file)))
